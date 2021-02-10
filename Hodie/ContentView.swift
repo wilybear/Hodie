cd9b97972e08756f29a862b688abdc98d207aecf
wilybear <samerj9712@gmail.com>
2021-02-10 23:35:49 +0900

    Initial commit

---
+++ Hodie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hodie
+//
+//  Created by 김현식 on 2021/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
