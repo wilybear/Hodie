fd4fdeab5cd8027d1ce507607d783b26a5709af7
wilybear <samerj9712@gmail.com>
2021-02-12 20:23:42 +0900

    Feat - CoreData added
    Initial Commit
    
    Scheduler, TodoTask (Model & ViewModels) : CoreData Entry extensions
    
    SerializableColor.swift : to save Colors in TodoTask
    
    ContentView.swift: test for core data

--- Hodie/ContentView.swift
+++ Hodie/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var context
+    @FetchRequest(fetchRequest: Scheduler.getSchedulers()) var schedulers: FetchedResults<Scheduler>
+    
+    @State private var newScheduler = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        NavigationView{
+            List{
+                HStack{
+                    TextField("New Scheduler", text: $newScheduler)
+                    Button(action: {
+                        let scheduler = Scheduler(context: context)
+                        scheduler.name_ = newScheduler
+                        scheduler.createdAt = Date()
+                        
+                        do {
+                            try context.save()
+                        }catch{
+                            print(error)
+                        }
+                        newScheduler = ""
+                    }, label: {
+                        Image(systemName: "plus.circle")
+                            .imageScale(.large)
+                    })
+                }
+                ForEach(schedulers){ scheduler in
+                    VStack{
+                        Text(scheduler.name_!)
+                            .font(.largeTitle)
+                        Text("\(scheduler.createdAt!)")
+                            .font(.caption)
+                    }
+                }
+            }.navigationTitle(Text("TODO TEST LIST"))
+            .navigationBarItems(leading: Button(action: {}, label: {
+                Image(systemName: "plus.circle")
+            }),trailing: EditButton())
+        }
     }
 }
---
+++ Hodie/Scheduler.swift
@@ -0,0 +1,34 @@
+//
+//  Scheduler.swift
+//  Hodie
+//
+//  Created by 김현식 on 2021/02/11.
+//
+
+import Foundation
+import CoreData
+
+extension Scheduler : Comparable{
+    public static func < (lhs: Scheduler, rhs: Scheduler) -> Bool {
+        lhs.createdAt! < rhs.createdAt!
+    }
+
+    static func fetchRequest(_ predicate: NSPredicate) -> NSFetchRequest<Scheduler> {
+        let request = NSFetchRequest<Scheduler>(entityName: "Scheduler")
+        request.sortDescriptors = [NSSortDescriptor(key:"createdAt", ascending: true)]
+        request.predicate = predicate
+        return request
+    }
+    
+    static func getSchedulers() -> NSFetchRequest<Scheduler> {
+        let request = NSFetchRequest<Scheduler>(entityName: "Scheduler")
+        request.sortDescriptors = [NSSortDescriptor(key:"createdAt", ascending: true)]
+        return request
+    }
+    
+    var todoTasks: Set<TodoTask>{
+        get{ todoTasks_ as? Set<TodoTask> ?? [] }
+        set{ todoTasks_ = newValue as NSSet}
+    }
+
+}
